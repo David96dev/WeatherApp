daada2574d898a09aa7b782f8b2777e0fe3106b7
yassinviv <157576550+yassinviv@users.noreply.github.com>
2024-03-22 13:54:22 +0100

    some changes
    some changes

--- WeatherApp_v1/WeatherApp_v1/obj/Debug/net8.0/WeatherApp_v1.AssemblyInfo.cs
+++ WeatherApp_v1/WeatherApp_v1/obj/Debug/net8.0/WeatherApp_v1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp_v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38dd3276badd713fa2ba631842676082d78e0b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac54a75c38f618f5fcce9c5c2bf945874fba782")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp_v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp_v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
